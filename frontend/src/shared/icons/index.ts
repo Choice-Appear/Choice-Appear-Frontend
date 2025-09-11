ff08fd06046ab9418fcc381e9f5e820934842bee
Joonsun Ryu <rjs7289@gmail.com>
2025-09-11 14:18:04 +0900

    style: lucide-react 아이콘에 대한 barren import 설정

---
+++ frontend/src/shared/icons/index.ts
@@ -0,0 +1,5 @@
+export { Menu } from 'lucide-react';
+export { Search } from 'lucide-react';
+export { Bookmark } from 'lucide-react';
+export { UserRound } from 'lucide-react';
+export { ShoppingCart } from 'lucide-react';
