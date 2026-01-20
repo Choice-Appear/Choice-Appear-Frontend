65b8922395f96c8dead045190458f3433202a4ff
Joonsun Ryu <rjs7289@gmail.com>
2026-01-20 14:10:11 +0900

    design: 사이드바 버튼 수정
    - 상하좌우 margin 부여
    - div 태그에서 link 태그로 수정

--- frontend/src/features/sidebar/SidebarOptions.tsx
+++ frontend/src/features/sidebar/SidebarOptions.tsx
@@ -1,11 +1,13 @@
+import styles from './SidebarOptions.module.scss';
+import { Link } from 'react-router-dom';
+
 export const SidebarOptions = () => {
-  const categories = ['여성', '남성', '모자', '후드'];
   return (
-    <div>
-      <div>여성</div>
-      <div>남성</div>
-      <div>모자</div>
-      <div>후드</div>
+    <div className={styles.container}>
+      <Link to={'/women'}>여성</Link>
+      <Link to={'/men'}>남성</Link>
+      <Link to={'/cap'}>모자</Link>
+      <Link to={'/hoodie'}>후드</Link>
     </div>
   );
 };
