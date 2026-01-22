e9ae6c42283efc4301370fee6821a114e7e09305
Joonsun Ryu <rjs7289@gmail.com>
2026-01-23 01:37:09 +0900

    fix: global.scss를 import하도록 수정
    - App.scss를 import하던 것을 global.scss를 import 하도록 수정

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,12 +1,9 @@
-import '@/app/styles/App.scss';
+import '@/app/styles/global.scss';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 
 function App() {
-  return (
-    <RouterProvider router={router} />
-
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
