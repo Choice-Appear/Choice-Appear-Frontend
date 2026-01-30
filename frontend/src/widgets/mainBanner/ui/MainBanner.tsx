7e2f29a61a5dd31bc0de024a47d2c8255da0023c
Joonsun Ryu <rjs7289@gmail.com>
2026-01-30 16:15:49 +0900

    design: 메인 배너 이미지 수정
    - 메인 배너 슬라이드 3번째 이미지 수정
    - 미사용 사진 삭제

--- frontend/src/widgets/mainBanner/ui/MainBanner.tsx
+++ frontend/src/widgets/mainBanner/ui/MainBanner.tsx
@@ -1,7 +1,7 @@
 import styles from './MainBanner.module.scss';
 import main1 from '@/shared/assets/main1.png';
 import main2 from '@/shared/assets/main2.png';
-import main3 from '@/shared/assets/main2.png';
+import main3 from '@/shared/assets/main3.png';
 import { SliderIndicator } from '@/shared/ui/slideIndicator';
 import { useState, useEffect } from 'react';
 
