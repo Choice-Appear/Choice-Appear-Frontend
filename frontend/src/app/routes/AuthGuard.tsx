7dab561a141150bcf13adefc08ad98dc5600bcb4
Joonsun Ryu <rjs7289@gmail.com>
2026-02-25 23:54:30 +0900

    refactor: 리다이렉트 경로 정교화
    - 로그인 상태에서 로그인 페이지 접근 시 리다이렉트 경로 정교화

--- frontend/src/app/routes/AuthGuard.tsx
+++ frontend/src/app/routes/AuthGuard.tsx
@@ -1,20 +1,24 @@
 import { useEffect } from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
 const AuthGuard = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     const handler = (event: Event) => {
       const customEvent = event as CustomEvent<{ redirectTo: string }>;
       const redirectTo = customEvent.detail?.redirectTo || '/';
 
+      // 이미 로그인 페이지 접근 시
+      if (location.pathname === '/login') return;
+
       navigate(`/login?redirect=${redirectTo}`, { replace: true });
     };
 
     window.addEventListener('unauthorized', handler);
     return () => window.removeEventListener('unauthorized', handler);
-  }, [navigate]);
+  }, [navigate, location.pathname]);
 
   return <Outlet />;
 };
